5951f7ac2534dbe9a70298d1f8d573011d5e9446
William <william.pinto@lambda3.com.br>
2020-12-04 09:17:49 -0300

    Class 02:02_Breaking our topic into multiple partitions

--- src/main/java/curso_kafka/FraudDetectorService.java
+++ src/main/java/curso_kafka/FraudDetectorService.java
@@ -8,6 +8,35 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+/*
+ * In this class, we are gonna run this service twice, so it can run in paralel to deal with messages. First of all,
+ * since we already created a ECOMMERCE_NEW_ORDER consumer, if we run the command below we will see that this consumer
+ * has only one partition.
+ * 
+ *						 .\kafka-topics.bat --bootstrap-server localhost:9092 --describe
+ *
+ *The number of partitions is important because it will tell Kafka if it can divide the work between multiple instances
+ *of a service, or if it let's it being executed by a single instance. So, to get started, we have to create more partitions
+ *of this.
+ *
+ *To do so, let's execute the command bellow:
+ *
+ * 						.\kafka-topics.bat --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER --partitions 3
+ *
+ * Good. Now Kafka will have created 3 partitions for our topic. if we run describe again, we'll see it.
+ * 
+ * Now, let's execute the FraudDetectorService in two instances. When we do this, the first service to run will get all
+ * the available partitions, but when the second is executed, Kafka will redistribute the number of partitions between 
+ * both. 
+ * 
+ * But, if we execute it, we will see only ONE of the services will receive the messages.  Why? Kafka will use a algorithm 
+ * to know where to send the value, and that algorithm uses the key whe are sendind in NewOrderMain's ProduceRecord's constructor.
+ * 
+ * Since, right now, we are using the same key for all messages, It will always send the message to the same instance of a 
+ * service (GOD DAMN IT, MARSHAL!!!)
+ * 
+ * */
+
 public class FraudDetectorService {
 
 	public static void main(String[] args) {
