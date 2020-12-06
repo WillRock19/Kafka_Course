715a29969ea57c32335ee71f22113ca540af85a9
William <william.pinto@lambda3.com.br>
2020-12-06 17:10:03 -0300

    Class 02_02: Removing userId since it does not exists in the moment the order is being register (it will only exist later)

--- service-new-order/src/main/java/curso_kafka_ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/curso_kafka_ecommerce/NewOrderMain.java
@@ -18,12 +18,11 @@ public class NewOrderMain {
 				for(var i = 0; i < 10; i++) 
 				{
 					var key = UUID.randomUUID().toString();
-					var userId = UUID.randomUUID().toString();
 					var orderId = UUID.randomUUID().toString();
 					var amount = new BigDecimal(Math.random() * 5000 + 1);
 					var userEmail = Math.random() + "@email.com";
 					
-					var order = new Order(userId, orderId, amount, userEmail);
+					var order = new Order(orderId, amount, userEmail);
 					var emailMessage = "Be welcome! We are processing your order :)";
 
 					orderDispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
