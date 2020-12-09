7e32a6e429fd28889cb6b8020f0189509958f6f0
William <william.pinto@lambda3.com.br>
2020-12-09 16:39:56 -0300

    Class 02_03: Adding MessageAdapter to deserialize gson and removing unecessary code

--- common-kafka/src/main/java/curso_kafka/services/GsonDeserializer.java
+++ common-kafka/src/main/java/curso_kafka/services/GsonDeserializer.java
@@ -7,31 +7,16 @@ import org.apache.kafka.common.serialization.Deserializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class GsonDeserializer<T> implements Deserializer<T> 
-{
-	public static final String TYPE_CONFIG = "random_initial_value_of_type_config";
-	
-	private final Gson gson = new GsonBuilder().create();
-	private Class<T> type;
+import curso_kafka_ecommerce.Message;
 
-	@Override
-	public void configure(Map<String, ?> configs, boolean isKey) 
-	{
-		String typeName = String.valueOf(configs.get(TYPE_CONFIG));
-
-		try 
-		{
-			this.type = (Class<T>)Class.forName(typeName);
-		}
-		catch(ClassNotFoundException e) 
-		{
-			throw new RuntimeException("Type for deserialization does not exist in classpath", e);
-		}
-	}
+public class GsonDeserializer<T> implements Deserializer<Message> 
+{	
+	/* We'll use the same typeAdapter that we've used in the serializer*/
+	private final Gson gson = new GsonBuilder().registerTypeAdapter(Message.class, new MessageAdapter()).create();
 	
 	@Override
-	public T deserialize(String topic, byte[] data) 
+	public Message deserialize(String topic, byte[] data) 
 	{	
-		return gson.fromJson(new String(data), type);
+		return gson.fromJson(new String(data), Message.class);
 	}
 }
--- common-kafka/src/main/java/curso_kafka/services/MessageAdapter.java
+++ common-kafka/src/main/java/curso_kafka/services/MessageAdapter.java
@@ -2,14 +2,18 @@ package curso_kafka.services;
 
 import java.lang.reflect.Type;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+import curso_kafka_ecommerce.CorrelationId;
 import curso_kafka_ecommerce.Message;
 
-public class MessageAdapter implements JsonSerializer<Message>{
+public class MessageAdapter implements JsonSerializer<Message>, JsonDeserializer<Message>{
 
 	@Override
 	public JsonElement serialize(Message message, Type typeOfSrc, JsonSerializationContext context) 
@@ -23,6 +27,29 @@ public class MessageAdapter implements JsonSerializer<Message>{
 		/*Creating objects inside my json*/
 		jsonObject.add("payload", context.serialize(message.getPayload()));
 		jsonObject.add("correlationId", context.serialize(message.getCorrelationId()));
+		
+		return jsonObject;
+	}
+
+	@Override
+	public Message deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException 
+	{
+		var jsonObject = json.getAsJsonObject();
+		var payloadType = jsonObject.get("type").getAsString();
+		var correlationId = (CorrelationId) context.deserialize(jsonObject.get("correlationId"), CorrelationId.class);
+		
+		try 
+		{
+			/* This opens the change that someone send a class that we cannot work with... one approach to that
+			 * would be test the payloadType in some "list of accepted classes". Since we don't want to focus
+			 * on this, we are keeping this simple. */
+			var payload = context.deserialize(jsonObject.get("payloadId"), Class.forName(payloadType));
+			return new Message(correlationId, payload);
+		} 
+		catch (JsonParseException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
